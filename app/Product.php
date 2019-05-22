815ee1bdb3c2cf0c21a11177430ed0e76124d67e
fadwa <fadwaana99@gmail.com>
2019-05-22 11:42:56 +0200

    Product proberties

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+     const AVAILABL_PRODUCT   = 'available';
+     const UNAVAILABL_PRODUCT = 'unavailable';
+
+
+      protected $fillable = [
+
+                    'name',
+                    'description',
+                    'quantity',
+                    'status', // only two values (0,1)
+                    'image',
+                    'seller_id'
+
+      ]; //mass assignment
+
+
+
+      public function isAvailable(){
+
+        return $this->status == Product::AVAILABL_PRODUCT;
+      }
 }
