ca7a5f67bb3a85df68b14efc6635760db92f2b5a
fadwa <fadwaana99@gmail.com>
2019-05-22 11:37:12 +0200

    making controllers and models

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,37 @@
+
 <?php
 
 use Illuminate\Http\Request;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
+* Buyer
+*show me the index page and show page only
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('buyers','Buyer\BuyerController',['only'=>['index','show']]);
+
+/*
+* Category
+* show me the all pages except the create and edit pages
+*/
+Route::resource('categories','Category\CategoryController',['except'=>['create','edit']]);
+/*
+* Product
+*/
+Route::resource('products','Product\ProductController',['only'=>['index','show']]); //show me the index page and show page only
+
+/*
+* Seller
+*/
+Route::resource('sellers','Seller\SellerController',['only'=>['index','show']]); //show me the index page and show page only
+
+
+/*
+* transaction
+*/
+Route::resource('transaction','Transaction\TransactionController',['only'=>['index','show']]); //show me the index page and show page only
+
+/*
+* user
+*/
+Route::resource('users','User\UserController',['except'=>['create','edit']]); //show me the index page and show page only
